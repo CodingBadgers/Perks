aaace18337d686a6940abe13fc9323fd15188da5
n3wton <n3wton@9b344ccc-9d30-4bac-a705-4099eefdac14>
2013-03-29 15:57:46 +0000

    [bTransported] Added teleport protection
    git-svn-id: https://mcbadgercraft.svn.cloudforge.com/perks@4 9b344ccc-9d30-4bac-a705-4099eefdac14

--- bTransported/src/uk/codingbadgers/btransported/bTransported.java
+++ bTransported/src/uk/codingbadgers/btransported/bTransported.java
@@ -1,13 +1,25 @@
 package uk.codingbadgers.btransported;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
 import uk.codingbadgers.bFundamentals.module.Module;
+import uk.codingbadgers.btransported.listeners.PlayerTeleportListener;
 
 /**
  * The Class bTransported.
  * Main entry point to the module
  */
 public class bTransported extends Module {
+	
+	private FileConfiguration m_teleportationConfiguration = null;
 	
 	/**
 	 * This is called when the module is unloaded
@@ -23,6 +35,116 @@ public class bTransported extends Module {
 	 */
 	@Override
 	public void onEnable() {	
-		log(Level.INFO,  getName() + " version " + getVersion() + " disabled.");
+		
+		loadLanguageFile();
+		createConfigurationFiles();
+		
+		// Register the player teleport listener
+		register(new PlayerTeleportListener(this));
+		
+		log(Level.INFO,  getName() + " version " + getVersion() + " enabled.");
+	}
+
+	/**
+	 * 
+	 */
+	private void createConfigurationFiles() {
+		
+		final File folder = this.getDataFolder();
+		
+		// create the dangerous block configuration file
+		createTeleportConfiguration(folder);
+		
+	}
+	
+	/**
+	 * 
+	 */
+	private void createTeleportConfiguration(final File folder)
+	{
+		File configFile = new File(folder + File.separator + "teleporting.yml");
+		
+		// If the configuration file doesnt exist, make a new one and populate it with default values
+		if (!configFile.exists()) {
+			try {
+				configFile.createNewFile();
+			} catch (IOException exception) {
+				log(Level.SEVERE, "Failed to create teleporting.yml");
+				exception.printStackTrace();
+				return;
+			}
+			
+			// Create a Yaml configuration for the given config file
+			m_teleportationConfiguration = YamlConfiguration.loadConfiguration(configFile);
+			
+			// Add the default blocks
+			List<Integer> blackListedBlocks = new ArrayList<Integer>();
+			blackListedBlocks.add(Material.LAVA.getId());
+			blackListedBlocks.add(Material.CACTUS.getId());
+			blackListedBlocks.add(Material.FIRE.getId());
+						
+			m_teleportationConfiguration.addDefault("dangerous-blocks", blackListedBlocks);
+			
+			List<Integer> safeBlocks = new ArrayList<Integer>();
+			safeBlocks.add(Material.AIR.getId());
+			safeBlocks.add(Material.WATER.getId());
+			safeBlocks.add(Material.SIGN.getId());
+			safeBlocks.add(Material.SIGN_POST.getId());
+			safeBlocks.add(Material.THIN_GLASS.getId());
+			safeBlocks.add(Material.LONG_GRASS.getId());
+			safeBlocks.add(Material.DEAD_BUSH.getId());
+			safeBlocks.add(Material.RED_ROSE.getId());
+			safeBlocks.add(Material.YELLOW_FLOWER.getId());
+			safeBlocks.add(Material.SAPLING.getId());
+			safeBlocks.add(Material.RAILS.getId());
+			safeBlocks.add(Material.ACTIVATOR_RAIL.getId());
+			safeBlocks.add(Material.DETECTOR_RAIL.getId());
+			safeBlocks.add(Material.POWERED_RAIL.getId());
+			
+			m_teleportationConfiguration.addDefault("safe-blocks", safeBlocks);
+			
+			m_teleportationConfiguration.addDefault("maximum-fall-distance", 5);
+
+			List<String> teleportcommandworlds = new ArrayList<String>();
+			m_teleportationConfiguration.addDefault("disable-in-world.teleport-command", teleportcommandworlds);
+
+			List<String> teleportenderpearlworlds = new ArrayList<String>();
+			teleportenderpearlworlds.add("world");
+			m_teleportationConfiguration.addDefault("disable-in-world.teleport-enderpearl", teleportenderpearlworlds);
+			
+			List<String> teleportendportalworlds = new ArrayList<String>();
+			m_teleportationConfiguration.addDefault("disable-in-world.teleport-endportal", teleportendportalworlds);
+			
+			List<String> teleportnetherportalworlds = new ArrayList<String>();
+			m_teleportationConfiguration.addDefault("disable-in-world.teleport-neatherportal", teleportnetherportalworlds);
+			
+			List<String> teleportpluginworlds = new ArrayList<String>();
+			m_teleportationConfiguration.addDefault("disable-in-world.teleport-plugin", teleportpluginworlds);
+			
+			// Save the changes
+			m_teleportationConfiguration.options().copyDefaults(true);
+			try { 
+				m_teleportationConfiguration.save(configFile); 
+			} catch (IOException exception) {
+				log(Level.SEVERE, "Failed to save teleporting.yml");
+				exception.printStackTrace();
+				return;
+			}
+			
+			log(Level.INFO, "Created the teleporting configuration file.");
+		}
+		else {
+			// configuration already exists, so just load that one
+			m_teleportationConfiguration = YamlConfiguration.loadConfiguration(configFile);
+		}
+		
+	}
+
+	/**
+	 * Get the dangerous block configuration file
+	 * @return The dangerous block configuration file
+	 */
+	public FileConfiguration getTeleportationConfig() {
+		return m_teleportationConfiguration;
 	}
 }
---
+++ bTransported/src/uk/codingbadgers/btransported/listeners/PlayerTeleportListener.java
@@ -0,0 +1,130 @@
+package uk.codingbadgers.btransported.listeners;
+
+import java.util.List;
+
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
+
+import uk.codingbadgers.bFundamentals.module.Module;
+import uk.codingbadgers.btransported.bTransported;
+
+public class PlayerTeleportListener implements Listener {
+	
+	/**
+	 * The bTransported module, used for access to configuration files and such
+	 */
+	private bTransported m_module = null;
+	
+	/**
+	 * Class constructor
+	 * @param module The bTransported module
+	 */
+	public PlayerTeleportListener(bTransported module) {
+		m_module = module;
+	}
+
+	/**
+	 * The player teleport event callback
+	 * @param event The event to process
+	 */
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onPlayerTeleport(PlayerTeleportEvent event) {
+		
+		final Player player = event.getPlayer();
+		final TeleportCause cause = event.getCause();
+		final Location from = event.getFrom();
+		final Location destination = event.getTo();
+				
+		// If the player is not in creative and the block below them is unsafe, cancel the teleport
+		if (player.getGameMode() != GameMode.CREATIVE && !isTeleportSafe(player, destination)) {
+			event.setCancelled(true);
+			return;
+		}
+		
+		// If the player is not in creative and the teleport cause is blacklisted in that world cancel
+		if (player.getGameMode() != GameMode.CREATIVE && isBlacklistedTeleport(player, from, cause)) {
+			event.setCancelled(true);
+			return;
+		}
+	
+	}
+	
+	private boolean isBlacklistedTeleport(final Player player, final Location location, final TeleportCause cause) {
+		
+		FileConfiguration teleportConfiguration = m_module.getTeleportationConfig();
+		
+		String configNode = null;
+		switch (cause)
+		{
+		case COMMAND:
+			configNode = "disable-in-world.teleport-command";
+			break;
+		case ENDER_PEARL:
+			configNode = "disable-in-world.teleport-enderpearl";
+			break;
+		case END_PORTAL:
+			configNode = "disable-in-world.teleport-endportal";
+			break;
+		case NETHER_PORTAL:
+			configNode = "disable-in-world.teleport-neatherportal";
+			break;
+		case PLUGIN:
+			configNode = "disable-in-world.teleport-plugin";
+			break;
+		default:
+			return false;
+		}
+		
+		List<String> worlds = teleportConfiguration.getStringList(configNode);
+		if (worlds.contains(location.getWorld().getName())) {
+			Module.sendMessage("bTransported", player, m_module.getLanguageValue("CANCEL-TELEPORT-TYPE-BLACKLISTED"));
+			return true;
+		}
+		
+		return false;
+	}
+
+	private boolean isTeleportSafe(final Player player, final Location destination) {
+		
+		final Block block = destination.getBlock();
+
+		FileConfiguration teleportConfiguration = m_module.getTeleportationConfig();
+		
+		List<Integer> whitelistedblocks = teleportConfiguration.getIntegerList("safe-blocks");
+		if (!whitelistedblocks.contains(block.getTypeId())) {
+			Module.sendMessage("bTransported", player, m_module.getLanguageValue("CANCEL-TELEPORT-NONSAFE"));
+			return false;
+		}
+
+		if (!player.isFlying()) {
+			int maxFallDistance = teleportConfiguration.getInt("maximum-fall-distance");
+			Location hightestBlock = new Location(destination.getWorld(), destination.getX(), destination.getY(), destination.getBlockZ());
+			while (hightestBlock.getBlock().getType() == Material.AIR) {
+				hightestBlock = hightestBlock.add(0.0, -1.0, 0.0);
+				maxFallDistance--;
+				if (maxFallDistance < 0) {
+					Module.sendMessage("bTransported", player, m_module.getLanguageValue("CANCEL-TELEPORT-FALL-DISTANCE"));
+					return false;
+				}
+			}
+			
+			List<Integer> blacklistedblocks = teleportConfiguration.getIntegerList("dangerous-blocks");
+			if (blacklistedblocks.contains(hightestBlock.getBlock().getTypeId())) {
+				Module.sendMessage("bTransported", player, m_module.getLanguageValue("CANCEL-TELEPORT-DANGEROUS"));
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+}
