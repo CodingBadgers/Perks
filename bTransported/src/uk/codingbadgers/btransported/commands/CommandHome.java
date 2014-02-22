217e94080d8d503cdcf0ddd68b814850b1922c20
samoatesgames <sam@samoatesgames.com>
2014-02-22 10:38:15 +0000

    Add max home config.
    Respect max home.
    Rewrite commands slightly (so remove | set is last param)
    Fixup tab completion

--- bTransported/src/uk/codingbadgers/btransported/commands/CommandHome.java
+++ bTransported/src/uk/codingbadgers/btransported/commands/CommandHome.java
@@ -1,15 +1,19 @@
 package uk.codingbadgers.btransported.commands;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import uk.codingbadgers.bFundamentals.bFundamentals;
@@ -34,6 +38,11 @@ public class CommandHome extends CommandPlaceBase {
      * The name of the anvil inventory used for naming a home.
      */
     public static final String ANVIL_INVENTORY_NAME = "BF-HOME-ANVIL";
+	
+	/**
+	 * A map of rank name and max number of homes that rank can have
+	 */
+	private Map<String, Integer> m_maxHomeCount;
 
     /**
      * Class constructor
@@ -45,10 +54,61 @@ public class CommandHome extends CommandPlaceBase {
 			module, 
 			ANVIL_INVENTORY_NAME, 
 			"home", 
-			"home | home <home name> | home <player name> <home name> | home set <name> | home remove <home name> | home remove <player name> <home name>"
+			"home | home <home name> | home <player name> <home name> | home <name> set | home <home name> remove | home <player name> <home name> remove"
 		);
 		m_homes = new HashMap<String, List<PlayerHome>>();
+		m_maxHomeCount = new HashMap<String, Integer>();
+		
+		loadConfig();
     }
+	
+	/**
+	 * Load the homes config
+	 */
+	private void loadConfig() {
+		
+		File file = new File(m_module.getDataFolder() + File.separator + "home.yml");
+		if (!file.exists()) {
+			// Create a new home config
+			try {
+				file.createNewFile();
+			}
+			catch (Exception ex) {
+				bFundamentals.log(Level.SEVERE, "Failed to create new home config!", ex);
+				return;
+			}
+			
+			// Set the default config options
+			FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+			config.set("maxhomes.default", 1);
+			
+			String[] groups = bFundamentals.getPermissions().getGroups();
+			for (String group : groups) {
+				config.set("maxhomes." + group, 1);
+			}
+			
+			// Save the new config
+			try {
+				config.save(file);
+			} catch (Exception ex) {
+				bFundamentals.log(Level.SEVERE, "Failed to save new home config!", ex);
+				return;
+			}
+		}
+		
+		// Load the config
+		FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+		
+		m_maxHomeCount.put("default", config.getInt("maxhomes.default"));
+		
+		String[] groups = bFundamentals.getPermissions().getGroups();
+		for (String group : groups) {
+			if (config.contains("maxhomes." + group)) {
+				m_maxHomeCount.put(group, config.getInt("maxhomes." + group));
+			}
+		}
+		
+	}
 
     @Override
     /**
@@ -76,9 +136,8 @@ public class CommandHome extends CommandPlaceBase {
 
         if (args.length == 0) {
 
-            // handle /home
+            // handle /home			
             handleHomeGUI(player);
-
             return true;
         }
         else if (args.length == 1) {
@@ -90,27 +149,41 @@ public class CommandHome extends CommandPlaceBase {
         }
         else if (args.length == 2) {
             
-            // handle /home <player name> <home name>
-            if (handleHomePlayerName(player, args[0], args[1])) {
-                return true;
-            }
-            
-            final String command = args[0];
+			final String command = args[1];
+			
+			// handle /home <home name> set
             if (command.equalsIgnoreCase("set")) {
-                addNewHome(player, player.getLocation(), args[1]);
+				
+				if (!Module.hasPermission(player, "btransported.home")) {
+					Module.sendMessage("Home", player, m_module.getLanguageValue("COMMAND-HOME-NO-PERMISSION"));
+					return true;
+				}
+				
+				if (getHomeCount(player) >= getMaxHomes(player)) {
+					Module.sendMessage("Home", player, m_module.getLanguageValue("COMMAND-HOME-REACHED-MAX"));
+					return true;
+				}
+				
+                addNewHome(player, player.getLocation(), args[0]);
                 return true;
             }
+			// handle /home <home name> remove
             else if (command.equalsIgnoreCase("remove")) {
-                handleHomeRemove(player.getName(), args[1]);
+                handleHomeRemove(player, player.getName(), args[0]);
                 return true;
             }
+            // handle /home <player name> <home name>
+			else if (handleHomePlayerName(player, args[0], args[1])) {
+                return true;
+            }            
             
         }
         else if (args.length == 3) {
             
-            final String command = args[0];
+			// handle /home <player name> <home name> remove
+            final String command = args[2];
             if (command.equalsIgnoreCase("remove")) {
-                handleHomeRemove(args[1], args[2]);
+                handleHomeRemove(player, args[0], args[1]);
                 return true;
             }
         }
@@ -118,45 +191,92 @@ public class CommandHome extends CommandPlaceBase {
         return false;
     }
     
-    private boolean handleHomeRemove(String playerName, String homeName) {
+	/**
+	 * Handle the remove home command
+	 * @param sender The player who is executing the command
+	 * @param playerName The owner of the home
+	 * @param homeName The name of the home
+	 * @return True on success, false otherwise
+	 */
+    private boolean handleHomeRemove(Player sender, String playerName, String homeName) {
+		
+		// If the sender is not the owner of the home check they have perms
+		if (!sender.getName().equalsIgnoreCase(playerName)) {
+			if (!Module.hasPermission(sender, "btransported.home.other.remove")) {
+				Module.sendMessage("Home", sender, m_module.getLanguageValue("COMMAND-HOME-NO-PERMISSION"));
+				return true;
+			}
+		}
         
         PlayerHome home = getHomeFromName(playerName, homeName);
         if (home == null) {
+			Module.sendMessage("Home", sender, m_module.getLanguageValue("COMMAND-HOME-NOT-FOUND"));
             return false;
         }
         
         List<PlayerHome> homes = m_homes.get(playerName);
         homes.remove(home);
         m_homes.put(playerName, homes);
-        
+		
+		Module.sendMessage("Home", sender, m_module.getLanguageValue("COMMAND-HOME-REMOVED"));
         return true;        
     }
     
+	/**
+	 * Handle the command /home <player name> <home name>
+	 * @param player
+	 * @param playerName
+	 * @param homeName
+	 * @return 
+	 */
     private boolean handleHomePlayerName(Player player, String playerName, String homeName) {
         
+		// If the sender is not the owner of the home check they have perms
+		if (!player.getName().equalsIgnoreCase(playerName)) {
+			if (!Module.hasPermission(player, "btransported.home.other")) {
+				Module.sendMessage("Home", player, m_module.getLanguageValue("COMMAND-HOME-NO-PERMISSION"));
+				return true;
+			}
+		}
+		
         OfflinePlayer homePlayer = Bukkit.getOfflinePlayer(playerName);
         if (!homePlayer.hasPlayedBefore()) {
+			Module.sendMessage("Home", player, m_module.getLanguageValue("COMMAND-HOME-COULD-NOT-FIND-PLAYER"));
             return false;
         }
         
         PlayerHome home = getHomeFromName(playerName, homeName);
         if (home == null) {
+			Module.sendMessage("Home", player, m_module.getLanguageValue("COMMAND-HOME-COULD-NOT-FIND-HOME"));
             return false;
         }
 
         return m_module.teleportOfflinePlayer(player, home.location);
     }
     
+	/**
+	 * Handle the command /home <home name>
+	 * @param player
+	 * @param homeName
+	 * @return 
+	 */
     private boolean handleHomeName(Player player, String homeName) {
         
         PlayerHome home = getHomeFromName(player.getName(), homeName);
         if (home == null) {
+			Module.sendMessage("Home", player, m_module.getLanguageValue("COMMAND-HOME-COULD-NOT-FIND-HOME"));
             return false;
         }
 
         return m_module.teleportOfflinePlayer(player, home.location);
     }
 
+	/**
+	 * Get a player home from a specified name and player
+	 * @param playername
+	 * @param homeName
+	 * @return 
+	 */
     private PlayerHome getHomeFromName(String playername, String homeName) {
         
         if (!m_homes.containsKey(playername)) {
@@ -173,7 +293,16 @@ public class CommandHome extends CommandPlaceBase {
         return null;
     }
     
+	/**
+	 * Open up the home gui
+	 * @param player The player to show it too
+	 */
     private void handleHomeGUI(Player player) {
+		
+		if (!Module.hasPermission(player, "btransported.home")) {
+			Module.sendMessage("Home", player, m_module.getLanguageValue("COMMAND-HOME-NO-PERMISSION"));
+			return;
+		}
 
         GuiInventory inventory = new GuiInventory(bFundamentals.getInstance());
         inventory.createInventory("Home Selection", 1);
@@ -191,20 +320,33 @@ public class CommandHome extends CommandPlaceBase {
             }
         }
 
-        Location playerLocation = player.getLocation();
+		if (getHomeCount(player) >= getMaxHomes(player)) {
+			// max home count reached
+			ItemStack item = new ItemStack(Material.BOOKSHELF);
+			String[] details = new String[4];
+			details[0] = "You have reached the maximum";
+			details[1] = "number of homes possible for your rank.";
+			details[2] = "Remove other homes or rank up to";
+			details[3] = "add more homes.";
 
-        // Add the writen book home creator
-        ItemStack item = new ItemStack(Material.BOOK_AND_QUILL);
-        String[] details = new String[3];
-        details[0] = "Create a new home at";
-        details[1] = playerLocation.getBlockX() + ", " + playerLocation.getBlockY() + ", " + playerLocation.getBlockZ();
-        details[2] = "in " + playerLocation.getWorld().getName();
+			final int LAST_SLOT = 8;
+			inventory.addMenuItem("Max Homes Reached", item, details, LAST_SLOT, null);
+		}
+		else {
+			Location playerLocation = player.getLocation();
 
-        final int LAST_SLOT = 8;
-        inventory.addMenuItem("New Home", item, details, LAST_SLOT, new NewHomeGuiCallback(m_module, player, this));
+			// Add the writen book home creator
+			ItemStack item = new ItemStack(Material.BOOK_AND_QUILL);
+			String[] details = new String[3];
+			details[0] = "Create a new home at";
+			details[1] = playerLocation.getBlockX() + ", " + playerLocation.getBlockY() + ", " + playerLocation.getBlockZ();
+			details[2] = "in " + playerLocation.getWorld().getName();
 
+			final int LAST_SLOT = 8;
+			inventory.addMenuItem("New Home", item, details, LAST_SLOT, new NewHomeGuiCallback(m_module, player, this));
+		}
+		
         inventory.open(player);
-
     }
 
     /**
@@ -273,47 +415,84 @@ public class CommandHome extends CommandPlaceBase {
 
         String playerName = sender.getName();
                 
-        //home remove <home name>
-        //home remove <player name> <home name>
-        if (args.length >= 2 && args[0].equalsIgnoreCase("remove")) {
-            
-            if (args.length == 3) {
-                playerName = args[1];
-            }
-            
-            if (!m_homes.containsKey(playerName)) {
-                return matches;
-            }
-            
-            List<PlayerHome> homes = m_homes.get(playerName);
-            for (PlayerHome home : homes) {
-                matches.add(home.name);
-            }            
-        }
         //home <home name>
+		//home <home name> set|remove
         //home <player name> <home name>
-        else if (args.length >= 1) {
+        if (args.length == 1 || args.length == 2) {
             
-            if (args.length == 2) {
+			if (args.length == 2) {
                 playerName = args[0];
+				matches.add("set");
+				matches.add("remove");
             }
+			else if (args.length == 1) {
+				// first param can be home name or player name
+				List<OfflinePlayer> players = m_module.matchPlayer(args[0], false);
+				for (OfflinePlayer other : players) {
+					matches.add(other.getName());
+				}
+			}
             
             if (!m_homes.containsKey(playerName)) {
                 return matches;
             }
             
             List<PlayerHome> homes = m_homes.get(playerName);
+			
+			final String homeLookup = args[args.length - 1];
+			
             for (PlayerHome home : homes) {
-                matches.add(home.name);
+				if (home.name.equalsIgnoreCase(homeLookup)) {
+					matches.add(home.name);
+					continue;
+				}
+				
+				if (home.name.toLowerCase().startsWith(homeLookup)) {
+					matches.add(home.name);
+				}
             }            
         }
+		else if (args.length == 3) {
+			// home <player name> <home name> remove
+			matches.add("remove");
+		}
 
         return matches;
     }
+	
+	/**
+	 * Get the maximum number of homes a player can have
+	 * @param player The player to get for
+	 * @return The number of homes
+	 */
+	private int getMaxHomes(Player player) {
+		
+		int maxHomes = m_maxHomeCount.get("default");
+		String group = bFundamentals.getPermissions().getPrimaryGroup((String)null, player.getName());
+		if (m_maxHomeCount.containsKey(group)) {
+			maxHomes = m_maxHomeCount.get(group);
+		}
+		
+		return maxHomes;
+	}
 
+	/**
+	 * Get the number of homes a player has
+	 * @param player The player to count the homes of
+	 * @return The number of homes the player has
+	 */
+	private int getHomeCount(Player player) {
+		
+		if (!m_homes.containsKey(player.getName())) {
+			return 0;
+		}
+		
+		return m_homes.get(player.getName()).size();
+	}
+	
 	@Override
 	/**
-	 * 
+	 * Called when an anvil naming gui is completed
 	 */
 	protected void onAnvilNameComplete(Player player, Location location, String name) {
 		addNewHome(player, location, name);
