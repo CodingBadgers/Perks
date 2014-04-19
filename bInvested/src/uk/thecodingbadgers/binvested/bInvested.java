4e683b2650307875167ab0ee86209e7183e5009f
samoatesgames <sam@samoatesgames.com>
2014-04-19 13:53:31 +0100

    Add a config to enable/disable specific parts of bInvested.

--- bInvested/src/uk/thecodingbadgers/binvested/bInvested.java
+++ bInvested/src/uk/thecodingbadgers/binvested/bInvested.java
@@ -1,6 +1,12 @@
 package uk.thecodingbadgers.binvested;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import uk.codingbadgers.bFundamentals.module.Module;
 import uk.thecodingbadgers.binvested.commands.*;
@@ -9,7 +15,9 @@ import uk.thecodingbadgers.binvested.listeners.*;
 public class bInvested extends Module {
 
 	private static bInvested instance = null;
-
+	
+	private YamlConfiguration m_settings = null;
+	
 	public static bInvested getInstance() {
 		return instance;
 	}
@@ -19,18 +27,82 @@ public class bInvested extends Module {
 		instance = this;
 		
 		loadLanguageFile();
+		loadConfig();
 		
-		registerCommand(new CommandFly());
-		registerCommand(new CommandCraft());
-		registerCommand(new CommandEnder());
-		registerCommand(new CommandAnvil());
-		registerCommand(new CommandEnchant());
+		if (m_settings.getBoolean("commands.fly")) {
+			registerCommand(new CommandFly());
+			register(new FlyListener(m_database));
+		}
 		
-		register(new FlyListener(m_database));
-		register(new ScubaListener());
-		register(new HungerListener(getConfig().getDouble("hunger.rate", 0.25f)));
+		if (m_settings.getBoolean("commands.workbench")) {
+			registerCommand(new CommandCraft());
+		}
+		
+		if (m_settings.getBoolean("commands.ender")) {
+			registerCommand(new CommandEnder());
+		}
+		
+		if (m_settings.getBoolean("commands.anvil")) {
+			registerCommand(new CommandAnvil());
+		}
+		
+		if (m_settings.getBoolean("commands.enchanting")) {
+			registerCommand(new CommandEnchant());
+		}
+		
+		if (m_settings.getBoolean("perk.scuba")) {
+			register(new ScubaListener());
+		}
+		
+		if (m_settings.getBoolean("perk.hunger")) {
+			register(new HungerListener(getConfig().getDouble("hunger.rate", 0.25f)));
+		}
 		
 		log(Level.INFO, getName() + " version " + getVersion() + " has been enabled successfully");
+	}
+	
+	/**
+	 * 
+	 */
+	private void loadConfig() {
+		
+		File configFile = new File(this.getDataFolder() + File.separator + "config.yml");
+		
+		// If the configuration file doesnt exist, make a new one and populate it with default values
+		if (!configFile.exists()) {
+			try {
+				configFile.createNewFile();
+			} catch (IOException exception) {
+				log(Level.SEVERE, "Failed to create config.yml");
+				exception.printStackTrace();
+				return;
+			}
+			
+			// Create a Yaml configuration for the given config file
+			m_settings = YamlConfiguration.loadConfiguration(configFile);
+			
+			m_settings.addDefault("commands.fly", true);
+			m_settings.addDefault("commands.workbench", true);
+			m_settings.addDefault("commands.anvil", true);
+			m_settings.addDefault("commands.enchanting", true);
+			m_settings.addDefault("commands.ender", true);
+			m_settings.addDefault("perk.scuba", true);
+			m_settings.addDefault("perk.hunger", true);
+			
+			try {
+				m_settings.save(configFile);
+			} catch (Exception ex) {
+				log(Level.SEVERE, "Failed to create config.yml");
+				ex.printStackTrace();
+			}
+			
+			log(Level.INFO, "Created the bInvested configuration file.");
+		}
+		else {
+			// configuration already exists, so just load that one
+			m_settings = YamlConfiguration.loadConfiguration(configFile);
+		}
+		
 	}
 
 	@Override
